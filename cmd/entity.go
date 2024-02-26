065b8941d23724819b0e854d7d2735e40d3ed62c
Harry Zhu <harryzhu@westwin.com>
2024-02-26 16:47:42 +0800

    -

--- cmd/entity.go
+++ cmd/entity.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+
 	"github.com/klauspost/compress/zstd"
 )
 
@@ -43,140 +44,197 @@ func (ett *Entity) OpenZipTempFile() *Entity {
 		zstd.WithEncoderLevel(ett.ZipLevel),
 		zstd.WithEncoderCRC(false))
 
-		var err error
-		ett.ZipTempFileHandler, err = os.Create(ett.ZipTempFile)
-		if err != nil {
-			ett.ZipTempFileHandler.Close()
-			FatalError(err)
-		}
-
-		ett.ZipWriter = zip.NewWriter(ett.ZipTempFileHandler)
-		ett.ZipWriter.RegisterCompressor(zstd.ZipMethodWinZip, compr)
-		return ett
-	}
-
-	func (ett *Entity) CloseZipTempFile() {
-		ett.ZipWriter.Close()
+	var err error
+	ett.ZipTempFileHandler, err = os.Create(ett.ZipTempFile)
+	if err != nil {
 		ett.ZipTempFileHandler.Close()
-
-		err := os.Rename(ett.ZipTempFile, ett.OutputFullPath)
 		FatalError(err)
 	}
 
-	func (ett *Entity) WithZipLevel(n int) *Entity {
-		cLevel := zstd.SpeedDefault
-		switch n {
-		case 0:
-			cLevel = zstd.SpeedFastest
-		case 1:
-			cLevel = zstd.SpeedDefault
-		case 6:
-			cLevel = zstd.SpeedBetterCompression
-		case 9:
-			cLevel = zstd.SpeedBestCompression
-		default:
-			cLevel = zstd.SpeedDefault
+	ett.ZipWriter = zip.NewWriter(ett.ZipTempFileHandler)
+	ett.ZipWriter.RegisterCompressor(zstd.ZipMethodWinZip, compr)
+	return ett
+}
+
+func (ett *Entity) CloseZipTempFile() {
+	ett.ZipWriter.Close()
+	ett.ZipTempFileHandler.Close()
+
+	err := os.Rename(ett.ZipTempFile, ett.OutputFullPath)
+	FatalError(err)
+}
+
+func (ett *Entity) WithZipLevel(n int) *Entity {
+	cLevel := zstd.SpeedDefault
+	switch n {
+	case 0:
+		cLevel = zstd.SpeedFastest
+	case 1:
+		cLevel = zstd.SpeedDefault
+	case 6:
+		cLevel = zstd.SpeedBetterCompression
+	case 9:
+		cLevel = zstd.SpeedBestCompression
+	default:
+		cLevel = zstd.SpeedDefault
+	}
+
+	ett.ZipLevel = cLevel
+	return ett
+}
+
+func (ett *Entity) SetZipFileMap() *Entity {
+	finfo, err := os.Stat(ett.InputFullPath)
+	FatalError(err)
+
+	var zfp map[string]string
+	if !finfo.IsDir() {
+		zfp = make(map[string]string, 1)
+		zfp[ett.InputFullPath] = filepath.Base(ett.InputFullPath)
+	} else {
+		fpathSkip := filepath.ToSlash(filepath.Dir(ett.InputFullPath))
+		nameInZip := ""
+		zfp = make(map[string]string, 4096)
+		var walkFunc = func(p string, info os.FileInfo, err error) error {
+			p = AbsToSlash(p)
+
+			nameInZip = strings.Trim(strings.Replace(p, fpathSkip, "", 1), "/")
+			nameInZip = filepath.ToSlash(nameInZip)
+			if nameInZip != "" && nameInZip != "." && nameInZip != ".." {
+				zfp[p] = nameInZip
+			}
+			return nil
 		}
-
-		ett.ZipLevel = cLevel
-		return ett
+		err = filepath.Walk(ett.InputFullPath, walkFunc)
+		FatalError(err)
 	}
 
-	func (ett *Entity) SetZipFileMap() *Entity {
-		finfo, err := os.Stat(ett.InputFullPath)
+	ett.ZipFileMap = zfp
+	return ett
+}
+
+func (ett *Entity) Compress() *Entity {
+	var header *zip.FileHeader
+	for abspath, zipname := range ett.ZipFileMap {
+
+		fmt.Println(zipname)
+
+		finfo, err := os.Stat(abspath)
 		FatalError(err)
 
-		var zfp map[string]string
-		if !finfo.IsDir() {
-			zfp = make(map[string]string, 1)
-			zfp[ett.InputFullPath] = filepath.Base(ett.InputFullPath)
+		fp, err := os.Open(abspath)
+		FatalError(err)
+
+		header, err = zip.FileInfoHeader(finfo)
+		if err != nil {
+			FatalError(err)
 		} else {
-			fpathSkip := filepath.ToSlash(filepath.Dir(ett.InputFullPath))
-			nameInZip := ""
-			zfp = make(map[string]string, 64)
-			var walkFunc = func(p string, info os.FileInfo, err error) error {
-				p = AbsToSlash(p)
+			header.Name = zipname
+		}
 
-				nameInZip = strings.Trim(strings.Replace(p, fpathSkip, "", 1), "/")
-				nameInZip = filepath.ToSlash(nameInZip)
-				if nameInZip != "" && nameInZip != "." && nameInZip != ".." {
-					zfp[p] = nameInZip
-				}
-				return nil
-			}
-			err = filepath.Walk(ett.InputFullPath, walkFunc)
+		header.Method = zstd.ZipMethodWinZip
+
+		w, err := ett.ZipWriter.CreateHeader(header)
+		FatalError(err)
+
+		if !finfo.IsDir() {
+			_, err = io.Copy(w, fp)
 			FatalError(err)
 		}
 
-		ett.ZipFileMap = zfp
-		return ett
 	}
 
-	func (ett *Entity) Compress() *Entity {
-		var header *zip.FileHeader
-		for abspath, zipname := range ett.ZipFileMap {
+	return ett
+}
 
-			fmt.Println(zipname)
+func (ett *Entity) Decompress() *Entity {
+	fh, err := os.Open(ett.InputFullPath)
+	FatalError(err)
 
-			finfo, err := os.Stat(abspath)
-			FatalError(err)
+	finfo, _ := fh.Stat()
 
-			fp, err := os.Open(abspath)
-			FatalError(err)
+	ett.UnzipReader, err = zip.NewReader(fh, finfo.Size())
+	FatalError(err)
 
-			header, err = zip.FileInfoHeader(finfo)
-			if err != nil {
-				FatalError(err)
-			} else {
-				header.Name = zipname
-			}
+	decomp := zstd.ZipDecompressor(
+		zstd.WithDecoderConcurrency(numCPU),
+	)
 
-			header.Method = zstd.ZipMethodWinZip
+	ett.UnzipReader.RegisterDecompressor(zstd.ZipMethodWinZip, decomp)
 
-			w, err := ett.ZipWriter.CreateHeader(header)
-			FatalError(err)
+	var dstPath, dstDir string
 
-			if !finfo.IsDir() {
-				_, err = io.Copy(w, fp)
-				FatalError(err)
-			}
-
+	for _, fzip := range ett.UnzipReader.File {
+		dstPath = filepath.Join(ett.OutputFullPath, fzip.Name)
+		dstPath = filepath.ToSlash(dstPath)
+		dstDir = filepath.Dir(dstPath)
+		if _, err := os.Stat(dstDir); err != nil {
+			os.MkdirAll(dstDir, os.ModePerm)
 		}
 
-		return ett
+		header := fzip.FileHeader
+		if header.FileInfo().IsDir() {
+			os.MkdirAll(dstPath, header.Mode())
+			continue
+		}
+
+		dst, _ := os.Create(dstPath)
+		funzip, err := fzip.Open()
+		PrintlnError(err)
+
+		if _, err := io.Copy(dst, funzip); err != nil {
+			PrintlnError(err)
+		} else {
+			fmt.Println(fzip.Name)
+		}
+		if err := funzip.Close(); err != nil {
+			PrintlnError(err)
+		}
+		dst.Close()
+
+		os.Chmod(dstPath, header.FileInfo().Mode())
+		os.Chtimes(dstPath, header.FileInfo().ModTime(), header.FileInfo().ModTime())
+
 	}
+	return ett
+}
 
-	func (ett *Entity) Decompress() *Entity {
-		fh, err := os.Open(ett.InputFullPath)
-		FatalError(err)
+func (ett *Entity) DecompressAsync() *Entity {
+	fh, err := os.Open(ett.InputFullPath)
+	FatalError(err)
 
-		finfo, _ := fh.Stat()
+	finfo, _ := fh.Stat()
 
-		ett.UnzipReader, err = zip.NewReader(fh, finfo.Size())
-		FatalError(err)
+	ett.UnzipReader, err = zip.NewReader(fh, finfo.Size())
+	FatalError(err)
 
-		decomp := zstd.ZipDecompressor(
-			zstd.WithDecoderConcurrency(numCPU),
-		)
+	decomp := zstd.ZipDecompressor(
+		zstd.WithDecoderConcurrency(numCPU),
+	)
 
-		ett.UnzipReader.RegisterDecompressor(zstd.ZipMethodWinZip, decomp)
+	ett.UnzipReader.RegisterDecompressor(zstd.ZipMethodWinZip, decomp)
 
-		var dstPath, dstDir string
+	var dstPath, dstDir string
+	var wg sync.WaitGroup
 
-		for _, fzip := range ett.UnzipReader.File {
-			dstPath = filepath.Join(ett.OutputFullPath, fzip.Name)
-			dstPath = filepath.ToSlash(dstPath)
-			dstDir = filepath.Dir(dstPath)
-			if _, err := os.Stat(dstDir); err != nil {
-				os.MkdirAll(dstDir, os.ModePerm)
-			}
+	for _, fzip := range ett.UnzipReader.File {
+		dstPath = filepath.Join(ett.OutputFullPath, fzip.Name)
+		dstPath = filepath.ToSlash(dstPath)
+		dstDir = filepath.Dir(dstPath)
+		if _, err := os.Stat(dstDir); err != nil {
+			os.MkdirAll(dstDir, os.ModePerm)
+		}
 
-			header := fzip.FileHeader
-			if header.FileInfo().IsDir() {
-				os.MkdirAll(dstPath, header.Mode())
-				continue
-			}
+		header := fzip.FileHeader
+		if header.FileInfo().IsDir() {
+			os.MkdirAll(dstPath, header.Mode())
+			continue
+		}
 
+		wg.Add(1)
+
+		go func(dstPath string, fzip *zip.File, header zip.FileHeader) {
+			defer wg.Done()
 
 			dst, _ := os.Create(dstPath)
 			funzip, err := fzip.Open()
@@ -194,68 +252,10 @@ func (ett *Entity) OpenZipTempFile() *Entity {
 
 			os.Chmod(dstPath, header.FileInfo().Mode())
 			os.Chtimes(dstPath, header.FileInfo().ModTime(), header.FileInfo().ModTime())
-			
-		}
-		return ett
+
+		}(dstPath, fzip, header)
+
+		wg.Wait()
 	}
-
-	func (ett *Entity) DecompressAsync() *Entity {
-		fh, err := os.Open(ett.InputFullPath)
-		FatalError(err)
-
-		finfo, _ := fh.Stat()
-
-		ett.UnzipReader, err = zip.NewReader(fh, finfo.Size())
-		FatalError(err)
-
-		decomp := zstd.ZipDecompressor(
-			zstd.WithDecoderConcurrency(numCPU),
-		)
-
-		ett.UnzipReader.RegisterDecompressor(zstd.ZipMethodWinZip, decomp)
-
-		var dstPath, dstDir string
-		var wg sync.WaitGroup
-
-		for _, fzip := range ett.UnzipReader.File {
-			dstPath = filepath.Join(ett.OutputFullPath, fzip.Name)
-			dstPath = filepath.ToSlash(dstPath)
-			dstDir = filepath.Dir(dstPath)
-			if _, err := os.Stat(dstDir); err != nil {
-				os.MkdirAll(dstDir, os.ModePerm)
-			}
-
-			header := fzip.FileHeader
-			if header.FileInfo().IsDir() {
-				os.MkdirAll(dstPath, header.Mode())
-				continue
-			}
-
-			wg.Add(1)
-
-			go func(dstPath string, fzip *zip.File, header zip.FileHeader){
-				defer wg.Done()
-
-				dst, _ := os.Create(dstPath)
-				funzip, err := fzip.Open()
-				PrintlnError(err)
-
-				if _, err := io.Copy(dst, funzip); err != nil {
-					PrintlnError(err)
-				} else {
-					fmt.Println(fzip.Name)
-				}
-				if err := funzip.Close(); err != nil {
-					PrintlnError(err)
-				}
-				dst.Close()
-
-				os.Chmod(dstPath, header.FileInfo().Mode())
-				os.Chtimes(dstPath, header.FileInfo().ModTime(), header.FileInfo().ModTime())
-
-			}(dstPath,fzip,header)
-			
-			wg.Wait()
-		}
-		return ett
-	}
+	return ett
+}
