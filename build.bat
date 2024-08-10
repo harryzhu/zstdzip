3f04320ff5c158b9cd405147a12e105d6fca8949
harryzhu <harry.chuh@gmail.com>
2024-08-10 19:42:29 +0800

    -

--- build.bat
+++ build.bat
@@ -1,1 +1,24 @@
-go build -o dist/zstdzip.exe -ldflags "-w -s" main.go
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build -o dist/linux/zstdzip -ldflags "-w -s" main.go
+
+SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=amd64
+go build -o dist/windows/zstdzip.exe -ldflags "-w -s" main.go
+
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=amd64
+go build -o dist/darwin_amd64/zstdzip -ldflags "-w -s" main.go
+
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=arm64
+go build -o dist/darwin_arm64/zstdzip -ldflags "-w -s" main.go
+
+7z.exe a dist/zstdzip_windows.zip dist/windows/zstdzip.exe
+7z.exe a dist/zstdzip_linux.zip dist/linux/zstdzip
+7z.exe a dist/zstdzip_mac.zip dist/darwin_arm64/zstdzip
+7z.exe a dist/zstdzip_mac_intel.zip dist/darwin_amd64/zstdzip
--- go.mod
+++ go.mod
@@ -3,11 +3,14 @@ module zstdzip
 go 1.21.4
 
 require (
+	github.com/klauspost/compress v1.17.7
+	github.com/spf13/cobra v1.8.0
+	github.com/zeebo/blake3 v0.2.3
+	github.com/zeebo/xxh3 v1.0.2
+)
+
+require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/klauspost/compress v1.17.7 // indirect
 	github.com/klauspost/cpuid/v2 v2.0.12 // indirect
-	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/zeebo/blake3 v0.2.3 // indirect
-	github.com/zeebo/xxh3 v1.0.2 // indirect
 )
