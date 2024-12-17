591178c293c8340e09f1bff8f6bae07dce1b4e5b
Harry Chuh <harry@Hair.local>
2024-12-17 15:10:52 +0800

    -

--- build.sh
+++ build.sh
@@ -1,1 +1,13 @@
-go build -o dist/zstdzip -ldflags "-w -s" main.go
+go build -o dist/macos_arm/zstdzip -ldflags "-w -s" main.go
+zip dist/macos_arm/zstdzip_macos_arm.zip dist/macos_arm/zstdzip
+
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o dist/macos_intel/zstdzip -ldflags "-w -s" main.go
+zip dist/macos_intel/zstdzip_macos_intel.zip dist/macos_intel/zstdzip
+
+
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o dist/linux_amd64/zstdzip -ldflags "-w -s" main.go
+zip dist/linux_amd64/zstdzip_linux_amd64.zip dist/linux_amd64/zstdzip
+
+
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o dist/windows_amd64/zstdzip.exe -ldflags "-w -s" main.go
+zip dist/windows_amd64/zstdzip_windows_amd64.zip dist/windows_amd64/zstdzip.exe
